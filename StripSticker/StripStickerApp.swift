622643132046911be6a4ca06f43061250cea4d8a
Jorge Acosta <jorge.acosta.freire@gmail.com>
2023-03-24 10:22:16 +0100

    Initial Commit

---
+++ StripSticker/StripStickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StripStickerApp.swift
+//  StripSticker
+//
+//  Created by Jorge Acosta Freire on 24/3/23.
+//
+
+import SwiftUI
+
+@main
+struct StripStickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
